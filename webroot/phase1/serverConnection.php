7e3f570d6e2acd70d046e0500828de9273d1fb8b
delanebrandy <delanebrandy13@outlook.com>
2022-04-27 17:25:13 +0100

    generalises server connection and js alert

---
+++ webroot/phase1/serverConnection.php
@@ -0,0 +1,15 @@
+<?php
+    function alert($message){
+        echo "<script type='text/javascript'>alert('$message');
+        </script>";
+    } 
+
+    $dbhost = getenv("MYSQL_SERVICE_HOST");
+    $dbport = getenv("MYSQL_SERVICE_PORT");
+    $dbuser = "root";
+    $dbpwd = "";
+    $dbname = "website";
+
+    $conn = mysqli_connect($dbhost, $dbuser, $dbpwd, $dbname) or
+    die('Connection to database failed: ' .$mysqli->connect_error);
+?>
